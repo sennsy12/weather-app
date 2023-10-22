a00d717ee28dbc8c13bf6925920c142c089d5f69
Alankc <122060247+sennsy12@users.noreply.github.com>
2023-10-22 13:01:40 +0200

    bedre
    fikset tid,dato

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const lastSearchElement = document.getElementById('last-search');
     const mainCard = document.querySelector('.main-card'); // Target the main card container
     const forecastCard = document.querySelector('.forecast-card'); // Target the forecast card container
-    
+
     let searchHistory = getSearchHistory();
+    // Initial API call and data population
+    fetchWeatherData('Oslo'); // You can replace 'New York' with the default city you want
 
     searchButton.addEventListener('click', () => {
         const cityName = cityInput.value;
@@ -84,7 +86,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function displayMainCard(weatherData) {
         mainCard.innerHTML = `
-            <h2>Current Weather</h2>
+            <h2> ${weatherData.name}, ${weatherData.sys.country}</h2>
+            <p>${new Date(weatherData.dt * 1000).toLocaleTimeString()}</p>
             ${weatherData.main ? `
                 <div class="weather-data">
                     <div class="data-icon"><i class="fas fa-thermometer-half"></i></div>
@@ -105,35 +108,52 @@ document.addEventListener('DOMContentLoaded', () => {
             ` : '<p>Weather information not available.</p>'}
         `;
     }
-
+    
     function displayForecastCard(forecastData) {
+        // Group forecast data by day
+        const dailyForecasts = forecastData.list.reduce((result, item) => {
+            const date = new Date(item.dt * 1000).toLocaleDateString();
+            if (!result[date]) {
+                result[date] = [];
+            }
+            result[date].push(item);
+            return result;
+        }, {});
+    
         forecastCard.innerHTML = `
-            <h2>Forecast Card (6-Day Forecast):</h2>
+            <h2>6-Day Forecast:</h2>
             <ul>
-                ${forecastData.list.slice(0, maxForecastDays).map(item => `
-                    <li>
-                        <h3>${new Date(item.dt * 1000).toDateString()}</h3>
-                        <div class="weather-data">
-                            <div class="data-icon"><i class="fas fa-thermometer-half"></i></div>
-                            <p>Temperature: ${item.main.temp.toFixed(2)}°C</p>
-                        </div>
-                        <div class="weather-data">
-                            <div class="data-icon"><i class="fas fa-sun"></i></div>
-                            <p>${item.weather[0].description}</p>
-                        </div>
-                        <div class="weather-data">
-                            <div class="data-icon"><i class="fas fa-tint"></i></div>
-                            <p>Humidity: ${item.main.humidity}%</p>
-                        </div>
-                        <div class="weather-data">
-                            <div class="data-icon"><i class="fas fa-cloud"></i></div>
-                            <p>Cloudiness: ${item.clouds.all}%</p>
-                        </div>
-                    </li>
-                `).join('')}
+                ${Object.entries(dailyForecasts).map(([date, items]) => {
+                    // Get the first item of the day
+                    const firstItem = items[0];
+                    return `
+                        <li>
+                            <h3>${new Date(firstItem.dt * 1000).toDateString()}</h3>
+                            <p>Time: ${new Date(firstItem.dt * 1000).toLocaleTimeString()}</p>
+                            <div class="weather-data">
+                                <div class="data-icon"><i class="fas fa-thermometer-half"></i></div>
+                                <p>Temperature: ${firstItem.main.temp.toFixed(2)}°C</p>
+                            </div>
+                            <div class="weather-data">
+                                <div class="data-icon"><i class="fas fa-sun"></i></div>
+                                <p>${firstItem.weather[0].description}</p>
+                            </div>
+                            <div class="weather-data">
+                                <div class="data-icon"><i class="fas fa-tint"></i></div>
+                                <p>Humidity: ${firstItem.main.humidity}%</p>
+                            </div>
+                            <div class="weather-data">
+                                <div class="data-icon"><i class="fas fa-cloud"></i></div>
+                                <p>Cloudiness: ${firstItem.clouds.all}%</p>
+                            </div>
+                        </li>
+                    `;
+                }).join('')}
             </ul>
         `;
     }
+    
+        
 
     function getSearchHistory() {
         const storedHistory = localStorage.getItem('searchHistory');
